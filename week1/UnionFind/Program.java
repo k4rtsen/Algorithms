ec3a84bf595df08ab162c01ec6578b227252c54c
kartsen <karen.ayv12599@gmail.com>
2022-09-30 09:56:22 +0300

    weighted QU + path compreccion

--- week1/UnionFind/Program.java
+++ week1/UnionFind/Program.java
@@ -1,14 +1,21 @@
 class UnionFind {
     private int[] id;
+    private int[] sz;
 
     public UnionFind(int N) {
         id = new int[N];
+        sz = new int[N];
         Program.initArr(id);
+        for (var i = 0; i < sz.length; i++) {
+            sz[i] = 1;
+        }
     }
 
     private int root(int i) {
-        while (i != id[i])
+        while (i != id[i]) {
+            id[i] = id[id[i]];
             i = id[i];
+        }
         return i;
     }
 
@@ -19,11 +26,21 @@ class UnionFind {
     public void union(int p, int q) {
         int i = root(p);
         int j = root(q);
-        id[i] = j;
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        }
+        else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
     }
 
-    public void print() {
+    public void printId() {
         Program.printArr(id);
+    }
+    public void printSize() {
+        Program.printArr(sz);
     }
 }
 
@@ -37,9 +54,12 @@ public class Program {
             int b = (int) (Math.random() * N);
             if (!uFind.connected(a, b))
                 uFind.union(a, b);
-            uFind.print();
+            System.out.printf("Array:\t");
+            uFind.printId();
             System.out.println("\ta: " + a + "\tb: " + b);
-            // System.out.println();
+            System.out.printf("Size: \t");
+            uFind.printSize();
+            System.out.println();
         }
     }
 
